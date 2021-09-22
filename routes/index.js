c2ec8886fdb72dd7d74b8c395000a6373a85b9b7
madhhuurrii <madhuriramakrishnan061@gmail.com>
2021-09-22 22:52:52 +0530

    passport authentication

--- routes/index.js
+++ routes/index.js
@@ -1,13 +1,36 @@
 const router = require('express').Router();
+const connectEnsureLogin = require('connect-ensure-login');
+const passport = require('passport');
+
 
 router.get('/',(req,res)=>{
+
     res.render('login');
 })
+router.post('/',(req,res,next) =>{
+    passport.authenticate('local',
+    (err,user,info) =>{
+        if(err){
+            return next(err);
+        }
+        if(!user){
+            return res.redirect('/?info='+info);
+        }
+        req.logIn(user, function(err){
+            if(err){
+                return next(err);
+            }
+            return res.redirect('/index');
+        });
 
-router.post('/',(req,res)=>{
-    let email = req.body.email;
-    let password = req.body.password;
-    res.send (`Email Address: ${ email } Password: ${ password }`);
-});
+    })
+    (req,res,next);
+})
+// router.post('/',(req,res)=>{
+//     let email = req.body.email;
+//     let password = req.body.password;
+//     res.send (`Email Address: ${ email } Password: ${ password }`);
+// });
+
 
 module.exports = router;
